0613b6f83453be20e286beb87c0157f5840c0753
JnLuis8a <128066020+JnLuis8a@users.noreply.github.com>
2026-04-29 20:40:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,15 @@ from flask import Flask, request, redirect
 import sqlite3
 from datetime import datetime
 import pandas as pd
-import os
 
 app = Flask(__name__)
-PASSWORD = "admin2026"  # CAMBIA A TU PASSWORD
+PASSWORD = "admin2026"
 
 @app.route('/')
-@app.route('/form')
 def form():
     return '''
 <!DOCTYPE html>
 <html>
 <head>
-<title>Medi-Ticket</title>
-<meta name="viewport" content="width=device-width">
-<style>
-body{font-family:'Segoe
+<title>Reportar Equipo</title>
+<meta name="viewport
